3ddda3cd5628b6a86cc6538909c29fc5fd547d95
Alireza Miryazdi <seyed.alireza.miryazdi@gmail.com>
2023-01-25 14:14:39 +0330

    Updated main

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 /*
-#cgo pkg-config: --define-variable=prefix=C:/gstreamer/1.0/mingw_x86_64 gstreamer-1.0
+#cgo pkg-config: gstreamer-1.0
 #cgo CFLAGS: -I${SRCDIR}
 #include "gstexample.h"
 */
